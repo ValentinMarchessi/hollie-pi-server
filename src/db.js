168e9171fb7c1f10e387615c65c478e374c36835
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-23 20:12:27 -0300

    DB SSL CONFIG

--- src/db.js
+++ src/db.js
@@ -3,16 +3,26 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DATABASE_URL
+  DB_HOST, DB_PASSWORD, DB_USER, DB_PORT, DB_NAME
 } = process.env;
 
 console.log('Connecting sequelize to postgres...');
 
-const sequelize = new Sequelize(`${DATABASE_URL}`, {
-	logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  ssl: true,
+const sequelize = new Sequelize({
+	database: DB_NAME,
+	username: DB_USER,
+	password: DB_PASSWORD,
+	host: DB_HOST,
+	port: DB_PORT,
+	dialect: 'postgres',
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
